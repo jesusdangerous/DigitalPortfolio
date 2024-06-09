1bbf09bc6e9e2d7950999a734790290ba83578b3
zonov <aleksandrzonov641@gmail.com>
2024-06-09 10:51:03 +0500

    authentication

--- DigitalPortfolioWeb/src/app/basic/components/login/login.component.ts
+++ DigitalPortfolioWeb/src/app/basic/components/login/login.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AuthService } from '../../services/auth/auth.service';
+import { NzNotificationService } from 'ng-zorro-antd/notification';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -6,5 +10,34 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent {
+	validateForm!: FormGroup;
 
+	constructor(private fb: FormBuilder,
+	  private authService: AuthService,
+	  private notification: NzNotificationService,
+	  private router: Router,
+	){
+    
+  }
+
+	ngOnInit(){
+		this.validateForm = this.fb.group({
+			username : [null, [Validators.required]],
+			password : [null, [Validators.required]],
+		})
+	}
+
+	submitForm(){
+			this.authService.login(this.validateForm.get(['userName'])!.value, this.validateForm.get(['password'])!.value)
+		  .subscribe(res=>{
+			  console.log(res)
+		}, error=>{
+			this.notification
+			.error(
+				'ERROR',
+				`Bad crendentials`,
+				{nzDuration: 5000}
+			)
+		})
+	}
 }
